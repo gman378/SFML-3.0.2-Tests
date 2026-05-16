1d770c4e37db2d893734d10167a35b3334c3a498
Derek Moreira <111708887+gman378@users.noreply.github.com>
2026-05-16 10:06:29 -0500

    main.cpp inicial

---
+++ Position Test/main.cpp
@@ -0,0 +1,47 @@
+#include <SFML/Graphics.hpp>
+#include <experimental/random>
+#include <iostream>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Teste de posicao");
+
+    sf::RectangleShape Object;
+
+    Object.setFillColor(sf::Color::Blue);
+    Object.setSize({100.f, 100.f});
+
+    float x = static_cast<float> (std::experimental::randint(10, (int) (window.getSize().x - Object.getSize().x)));
+
+    Object.setPosition({ x, 10.f});
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        Object.move({0.0f, 0.04f});
+
+        if(Object.getPosition().y > 1080.f)
+        {
+             float x = static_cast<float> (std::experimental::randint(10, (int) (window.getSize().x - Object.getSize().x)));
+            Object.setPosition({x, 10.f}); 
+            
+        }
+
+       // if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+        //{
+       //     Object.setPosition({x, 10.f});
+       // }
+        
+
+        window.clear();
+        window.draw(Object);
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}

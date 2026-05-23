4f4f906e71151d10904d37b04eae6e30823d7c01
Derek Moreira <111708887+gman378@users.noreply.github.com>
2026-05-22 21:08:42 -0500

    Novos comentarios

--- Position Test/main.cpp
+++ Position Test/main.cpp
@@ -11,7 +11,7 @@ int main()
     Object.setFillColor(sf::Color::Blue); //Aqui eu defino a cor do bloquinho.
     Object.setSize({100.f, 100.f}); //Aqi eu defino o tamanho do bloquinho.
 
-    float x = static_cast<float> (std::experimental::randint(10, (int) (window.getSize().x - Object.getSize().x)));
+    float x = static_cast<float> (std::experimental::randint(10, (int) (window.getSize().x - Object.getSize().x))); //Aqui aonde eh definido o movimento do bloquinho, com o uso da biblioteca "experimental" eu consigo fazer o movimento dele ser aleatorio.
 
     Object.setPosition({ x, 10.f});
 
